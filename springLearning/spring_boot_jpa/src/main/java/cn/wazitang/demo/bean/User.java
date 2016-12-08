b95fbacf690a29400d24b4df965d6a6ac79bbba7
zcw <zcw1994@live.com>
2016-12-08 22:30:38 +0800

    网络太卡，临时推

---
+++ springLearning/spring_boot_jpa/src/main/java/cn/wazitang/demo/bean/User.java
@@ -0,0 +1,11 @@
+package cn.wazitang.demo.bean;
+
+import org.hibernate.annotations.Table;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Created by zcw on 2016/12/07.
+ */
+public class User {
+
+}
