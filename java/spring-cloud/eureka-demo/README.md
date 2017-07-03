0f4412b982183036bf97e6375544d5c964c0281e
zcw <chaowei.zeng@bolemart.com>
2017-07-03 19:05:37 +0800

    first spring cloud,eureka demo

---
+++ java/spring-cloud/eureka-demo/README.md
@@ -0,0 +1,5 @@
+# spring cloud demo --- eureka
+
+1. [forezp的个人博客](http://blog.csdn.net/forezp/article/details/69696915)
+
+1. [玄玉的博客](http://jadyer.cn/2017/01/19/springcloud-ribbon-feign/)
---
+++ java/spring-cloud/eureka-demo/eureka-client/src/main/java/cn/wazitang/demo/springcloud/eureka/client/EurekaClientApplication.java
@@ -0,0 +1,22 @@
+package cn.wazitang.demo.springcloud.eureka.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String index(){
+		return "SUCCESS";
+	}
+}
